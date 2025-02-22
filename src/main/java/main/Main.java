bc03e53c810618152b9091f6297f330900b288e9
AARomeraM <andresalejandro.romera817@comunidadunir.net>
2025-02-22 01:54:32 +0100

    Reubicación de las clases a su directorio correcto e implementacion de de los menus especificos en el menu principal

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,48 @@
+package main;
+
+
+import aplicaciones.ScannerGlobal;
+import static aplicaciones.MenuOpciones.pintarMenu;
+import static aplicaciones.MenuSuma.menuSuma;
+import static aplicaciones.Producto;
+import static aplicaciones.Division;
+
+/** Clase Main donde se gestiona el menu de la calculadora
+ * @author Luis Calzadilla / @LuisDavidCR
+ * @version 0.1
+ * @see aplicaciones.MenuOpciones
+ * @see aplicaciones.ScannerGlobal
+ * @see aplicaciones.MenuSuma
+ */
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int opcion = 0;
+
+        do {
+            opcion = pintarMenu();
+            switch(opcion) {
+                case 1:
+                    menuSuma();
+                    break;
+                case 2:
+                    System.out.println("Metodo Resta");
+                    break;
+                case 3:
+                    menu_producto();
+                    break;
+                case 4:
+                    menu_division();
+                    break;
+            }
+
+
+        }while(opcion != 5);
+
+        System.out.println("FIN DE PROCESO");
+        ScannerGlobal.leer.close();
+    }
+
+}
